36871ccad3cd8c327e51fb6061567a931f46785a
Vincent St-Amour <stamourv@racket-lang.org>
2015-07-24 15:33:31 -0500

    Move markparam tests to the markparam-test package.

---
+++ markparam-test/info.rkt
@@ -0,0 +1,8 @@
+#lang info
+
+(define collection "markparam")
+(define version "1.0")
+(define deps '(("base" #:version "6.2.900.4")
+               "markparam-lib"))
+(define build-deps '("eli-tester"))
+(define pkg-authors '(jay))
