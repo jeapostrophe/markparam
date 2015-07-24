135b5ecdeeae442d2d6d54a79857c5b27e520516
Vincent St-Amour <stamourv@racket-lang.org>
2015-07-24 15:41:20 -0500

    Add a markparam meta-package.

---
+++ markparam/info.rkt
@@ -0,0 +1,11 @@
+#lang info
+
+(define collection 'multi)
+
+(define deps '("markparam-lib"
+               "markparam-doc"))
+(define implies '("markparam-lib"
+                  "markparam-doc"))
+
+(define version "1.0")
+(define pkg-authors '(jay))
