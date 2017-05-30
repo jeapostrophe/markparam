c50ed5244f074240d0ccd89cf201530642854fed
Jay McCarthy <jay.mccarthy@gmail.com>
2017-05-30 13:40:19 -0400

    deps

--- markparam-test/info.rkt
+++ markparam-test/info.rkt
@@ -4,5 +4,6 @@
 (define version "1.0")
 (define deps '(("base" #:version "6.2.900.4")
                "markparam-lib"))
-(define build-deps '("eli-tester"))
+(define build-deps '("racket-index"
+                     "eli-tester"))
 (define pkg-authors '(jay))
